f70d1495dc3dfc8021768ce62c3396b80ea5d8c8
Tomasz Kostur <tomasz.kostur@gmail.com>
2022-03-13 09:04:18 +0100

    initial of python_module_written_in_rust

---
+++ main_test.py
@@ -0,0 +1,6 @@
+import module_written_in_rust
+out = module_written_in_rust.sum_as_string(5, 20)
+print(out)
+
+print(module_written_in_rust.__doc__)
+print(module_written_in_rust.sum_as_string.__doc__)
